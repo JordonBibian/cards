a992a33e297ff3ecae57fd4f3aa0cabfc0e433ae
Jordon Bibian <jordonbibian@gmail.com>
2023-03-21 04:36:55 -0700

    Initial Commit

---
+++ Cards Rework/Cards_ReworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cards_ReworkApp.swift
+//  Cards Rework
+//
+//  Created by Jordon Bibian on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Cards_ReworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
