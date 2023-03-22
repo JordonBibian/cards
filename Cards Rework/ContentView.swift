469ca27e9db409b28fd5c0d02fc460b0a0c0e52a
Jordon Bibian <jordonbibian@gmail.com>
2023-03-21 18:12:53 -0700

    Restoring changes again

--- Cards Rework/ContentView.swift
+++ Cards Rework/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var cards = [
+        Card(front: "What is 7+7?", back: "14"),
+        Card(front: "What is the airspeed velocity of an unladen swallow?", back: "African or European?"),
+        Card(front: "From what is cognac made?", back: "Grapes")
+        ]
+    @State var isShowingCardView = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            CardDeckView(cards: cards)
+            Button {
+                isShowingCardView = true
+            } label: {
+                Image(systemName: "plus")
+                    .font(.headline)
+                    .padding()
+                    .background(Color.orange)
+                    .clipShape(Circle())
+            }
+            .padding([.top, .trailing])
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
         }
-        .padding()
+        .sheet(isPresented: $isShowingCardView, onDismiss: { print(isShowingCardView) }) {
+            CreateCardView()
+        }
+        
     }
 }
 
